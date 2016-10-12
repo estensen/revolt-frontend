83abecc82380538ff7c6aab5a97233d6e63adb6c
Vemund M Santi <vemund@santi.no>
2016-10-12 22:06:57 +0200

    Start work on EpisodeAdmin

--- app/containers/EpisodeAdmin/index.js
+++ app/containers/EpisodeAdmin/index.js
@@ -10,9 +10,20 @@ import selectEpisodeAdmin from './selectors';
 import styles from './styles.css';
 
 export class EpisodeAdmin extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
   render() {
     return (
       <div className={styles.episodeAdmin}>
+        <h1>Opprett ny episode</h1>
+        <div className={styles.ShowAdmin}>
+          <div>
+            <form>
+              <button className={styles.submitButton} type="submit" value="Lagre">Lagre</button>
+            </form>
+          </div>
+          <div className={styles.episodePreview}>
+          </div>
+        </div>
       </div>
     );
   }
