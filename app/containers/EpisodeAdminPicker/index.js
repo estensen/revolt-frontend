c7048a615c3de6345b94449cbd327c45eec8287d
Vemund M Santi <vemund@santi.no>
2016-11-09 21:37:23 +0100

    Added ShowPicker component for displaying a list view of shows

--- app/containers/EpisodeAdminPicker/index.js
+++ app/containers/EpisodeAdminPicker/index.js
@@ -6,25 +6,55 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
-import selectEpisodeAdminPicker from './selectors';
+import { createStructuredSelector } from 'reselect';
+import {
+  selectShows,
+  selectShowsLoading,
+  selectShowsError,
+} from 'containers/Shows/selectors';
 import styles from './styles.css';
+
+import { loadShows } from 'containers/Shows/actions';
 
 import SelectInput from 'components/SelectInput';
 
 export class EpisodeAdminPicker extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  componentWillMount() {
+    this.props.loadShow();
+  }
+
   render() {
     return (
       <div className={styles.episodeAdminPicker}>
-        <SelectInput />
+        <SelectInput label={'Velg showet som tilhører episoden'} options={this.props.shows} />
       </div>
     );
   }
 }
 
-const mapStateToProps = selectEpisodeAdminPicker();
+EpisodeAdminPicker.propTypes = {
+  shows: React.PropTypes.oneOfType([
+    React.PropTypes.bool,
+    React.PropTypes.array,
+  ]),
+  loadShow: React.PropTypes.func.isRequired,
+};
+
+EpisodeAdminPicker.defaultProps = {
+  loading: false,
+  error: false,
+  shows: [],
+};
+
+const mapStateToProps = createStructuredSelector({
+  shows: selectShows(),
+  loading: selectShowsLoading(),
+  error: selectShowsError(),
+});
 
 function mapDispatchToProps(dispatch) {
   return {
+    loadShow: () => dispatch(loadShows()),
     dispatch,
   };
 }
