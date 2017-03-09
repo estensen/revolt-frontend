21f9df78d25b120f19f9d4edd0709e0972c77622
Vemund M Santi <vemund@santi.no>
2017-03-09 13:51:52 +0100

    Update chief radio editor and contact info

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -7,19 +7,29 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-
 import styles from './styles.css';
 
 function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.content}>
-        <p>Denne tjenesten tilbys av Studentmediene i Trondheim AS. Musikken er gjengitt med tilatelse fra TONO/NCB.</p>
+        <p>
+          Denne tjenesten tilbys av Studentmediene i Trondheim AS. Musikken er
+          gjengitt med tilatelse fra TONO/NCB.
+        </p>
         <p>Uautorisert lenking, videreføring eller kopiering er ulovlig.</p>
         <br />
         <p>Radioredaktør: Peter Wallumrød</p>
         <p>Ansvarlig redaktør: Håkon Pedersen</p>
         <br />
+        <p>
+          <a
+            className={styles.footerLink}
+            href="mailto:redaktor@radiorevolt.no"
+          >
+            Kontakt oss
+          </a>
+        </p>
         <p>
           <Link className={styles.footerLink} to="/om">
             Om oss
