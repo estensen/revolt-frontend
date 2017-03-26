0fbe4ff934cbe4ae9ccb0000a6fb8adc453b34f0
Vemund M Santi <vemund@santi.no>
2017-03-26 21:24:53 +0200

    Remove internationalization and translations

--- internals/generators/component/index.js
+++ internals/generators/component/index.js
@@ -29,11 +29,6 @@ module.exports = {
     name: 'wantCSS',
     default: true,
     message: 'Does it have styling?',
-  }, {
-    type: 'confirm',
-    name: 'wantMessages',
-    default: true,
-    message: 'Do you want i18n messages (i.e. will this component use text)?',
   }],
   actions: data => {
     // Generate index.js and index.test.js
@@ -58,17 +53,6 @@ module.exports = {
         abortOnFail: true,
       });
     }
-
-    // If they want a i18n messages file
-    if (data.wantMessages) {
-      actions.push({
-        type: 'add',
-        path: '../../app/components/{{properCase name}}/messages.js',
-        templateFile: './component/messages.js.hbs',
-        abortOnFail: true,
-      });
-    }
-
     return actions;
   },
 };
