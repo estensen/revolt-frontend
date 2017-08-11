49ca7666458118160ff0fd715056a4f810b70df0
Vemund M Santi <vemund@santi.no>
2017-08-11 16:47:42 +0200

    build: update polyfills

--- internals/testing/polyfills.js
+++ internals/testing/polyfills.js
@@ -6,17 +6,6 @@ if (typeof Promise === 'undefined') {
   window.Promise = require('promise/lib/es6-extensions.js');
 }
 
-// react-slick, used by antd, requires this to pass jest tests
-window.matchMedia =
-  window.matchMedia ||
-  function() {
-    return {
-      matches: false,
-      addListener: function() {},
-      removeListener: function() {},
-    };
-  };
-
 // fetch() polyfill for making API calls.
 require('whatwg-fetch');
 
