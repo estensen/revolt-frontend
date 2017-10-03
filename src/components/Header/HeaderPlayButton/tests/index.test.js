b64018e0650beb60f1d43440d1748037aed1d90c
Vemund M Santi <vemund@santi.no>
2017-10-03 02:58:26 +0200

    test: add tests for redux actions to HeaderLiveButton

--- src/components/Header/HeaderPlayButton/tests/index.test.js
+++ src/components/Header/HeaderPlayButton/tests/index.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { shallow, mount } from 'enzyme';
+import configureStore from 'redux-mock-store';
 
-import { HeaderPlayButton } from '../';
+import ConnectedHeaderPlayButton, { HeaderPlayButton } from '../';
+import { playLive } from 'components/Player/actions';
 
 describe('<HeaderPlayButton />', () => {
   it('renders correctly', () => {
@@ -19,5 +22,31 @@ describe('<HeaderPlayButton />', () => {
     const tree = mount(<HeaderPlayButton {...mockProps} />);
     tree.find('.playButton').simulate('click');
     expect(mockProps.playLive).toBeCalled();
+  });
+
+  it('dispatches action when clicked', () => {
+    const mockStore = configureStore();
+    const store = mockStore({});
+    const tree = mount(
+      <Provider store={store}>
+        <ConnectedHeaderPlayButton />
+      </Provider>,
+    );
+    tree.find('.playButton').simulate('click');
+    const actions = store.getActions();
+    expect(actions).toEqual([playLive()]);
+  });
+
+  it('dispatches action on keyPress', () => {
+    const mockStore = configureStore();
+    const store = mockStore({});
+    const tree = mount(
+      <Provider store={store}>
+        <ConnectedHeaderPlayButton />
+      </Provider>,
+    );
+    tree.find('.playButton').simulate('keyPress');
+    const actions = store.getActions();
+    expect(actions).toEqual([playLive()]);
   });
 });
