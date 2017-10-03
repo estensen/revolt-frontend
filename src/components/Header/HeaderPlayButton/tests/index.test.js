adc4ddf141056f47ce47b0bdc3b8654bb13df21f
Vemund M Santi <vemund@santi.no>
2017-10-03 02:39:54 +0200

    test: add interactivity test to HeaderPlayButton

--- src/components/Header/HeaderPlayButton/tests/index.test.js
+++ src/components/Header/HeaderPlayButton/tests/index.test.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import { HeaderPlayButton } from '../';
 
-const mockProps = {
-  playLive: () => {},
-};
-
 describe('<HeaderPlayButton />', () => {
   it('renders correctly', () => {
+    const mockProps = {
+      playLive: jest.fn(),
+    };
     const tree = shallow(<HeaderPlayButton {...mockProps} />);
     expect(tree).toMatchSnapshot();
+  });
+
+  it('calls playLive when clicked', () => {
+    const mockProps = {
+      playLive: jest.fn(),
+    };
+    const tree = mount(<HeaderPlayButton {...mockProps} />);
+    tree.find('.playButton').simulate('click');
+    expect(mockProps.playLive).toBeCalled();
   });
 });
